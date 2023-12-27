d003d4912b3828c5d3eedaf553a98be90ace9a00
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2023-12-27 11:56:49 +0300

    Update Stacks.c

--- Stacks.c
+++ Stacks.c
@@ -1,1 +1,74 @@
-Stacks.c
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+
+};
+
+struct node *top=0;
+
+void push (int x){
+
+    struct node *newnode;
+    newnode=(struct node *)malloc(sizeof(struct node));
+    newnode -> data = x;
+    newnode ->link = top;
+    top = newnode;
+
+}
+
+void display(){
+    struct node*temp;
+    temp=top;
+    if(top==0) {
+        printf(" List is empty. \n");
+    }
+    
+    else {
+        while (temp!=0){
+            printf("\n %d \n", temp -> data);
+            temp=temp->link;
+        }
+    }
+}
+void peek(){
+    if(top==0)
+    printf("List is empty.");
+    else{
+        printf("Top element is %d \n", top ->data);
+    
+    }
+}
+
+void pop(){
+    struct node *temp;
+    temp=top;
+    if(top==0){
+    printf("List is empty.");
+    }
+    
+    else {
+        printf("Pooped element is %d \n", top->data);
+        top =top->link;
+        free(temp);
+    
+    }
+}
+
+int main(){
+    push(7);
+    push(9);
+    push(15);
+    display();
+    peek();
+    pop();
+    peek();
+    display();
+
+    return 0;
+
+}
