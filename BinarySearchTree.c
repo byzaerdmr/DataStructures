f10a40f50b3733cc770f2777899ea604d7f0f246
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-04 12:29:31 +0300

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -45,3 +45,63 @@ struct BstNode* insertNode(struct BstNode* node, int value)
     }
     return node;
 }
+void postOrder(struct BstNode* root)
+{
+    if (root != NULL) {
+       postOrder(root->left);
+       postOrder(root->right);
+       printf("%d", root ->data); 
+    }
+}
+
+void inOrder(struct BstNode* root)
+{
+    if(root!=NULL) {
+        inOrder(root->left);
+        printf("%d", root->data);
+        inOrder(root ->right);
+    }
+}
+
+void preOrder(struct BstNode* root)
+{
+    if(root!=NULL){
+        printf("%d", root->data);
+        preOrder(root->left);
+        preOrder(root ->right);
+    }
+}
+
+struct BstNode* deleteNode(struct BstNode* root, int x)
+{
+    if(root==NULL)
+    return root;
+    
+    if(x > root->data){
+        root ->right= deleteNode(root -> right, x);
+    }
+    else if(x < root->data) {
+        root ->left = deleteNode(root->left, x);
+    }
+    else{
+        if(root -> left == NULL){
+            struct BstNode* temp = root ->right;
+            free(root);
+            return temp;
+       }
+        else if (root ->right == NULL){
+            struct BstNode* temp = root -> left;
+            free(root);
+            return temp;
+        }
+        struct BstNode* temp = root ->right;
+        while( temp->left !=NULL){
+            temp = temp->left;
+        }
+        root -> data = temp -> data;
+
+        root -> right = deleteNode(root->right, temp -> data);
+    }
+    return root;
+} 
+
