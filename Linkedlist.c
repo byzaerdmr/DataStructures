b0853a6196170a4ef15c028434a1c6687c90b703
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-06 13:48:43 +0300

    Update Linkedlist.c

--- Linkedlist.c
+++ Linkedlist.c
@@ -1,60 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-typedef struct list {
-    int info;
+typedef struct list{                                     //bağlı liste tanımı
+    int data;
     struct list* next;
-}list;
+}  list;
 
-list* first = NULL;
-list* last = NULL;
+list* first=NULL;
+list* last=NULL;
 
-int insert(int value) {
-    list* p = (list*)malloc(sizeof(list));
-    if (p == NULL) {
-        printf("Memory failed\n");
+int insert(int value){                         //bağlı listeye ekleme yapma ve size güncelleme
+    list* new = (list*)malloc(sizeof(list));
+    if (new == NULL){
+        printf("Memory failed \n");
         return -1;
     }
-    p->info = value;
-    p->next = NULL;
 
-    if (first != NULL) {
-        last->next = p;
-        last = p;
-    }
-    else {
-        first = p;
-        last = p;
+    new->data=value;
+        new->next=NULL;
 
+    if(first != NULL){    //liste boş değilse ekle ve son elemanı güncelle
+    last->next=new;
+    last=new; 
     }
-    return 0;
-}
 
-int display() {
-    list* p;
-    p = first;
-    if (p == NULL) {
-        printf("List is empty\n");
-        return -1;
-    }
-    while (p != NULL) {
-        printf("%d \n", p->info);
-        p = p->next;
+    else{                 // liste boşsa ilk elemanı ekle
+    first =new;
+    last=new;
     }
 
     return 0;
 }
 
+    int display(){   //bağlı listeyi yazdırma
 
-int main() {
-    int value, i;
+        list* new;
+        new= first;
+
+     if (new==NULL){
+        printf("List is empty \n");
+        return -1;
+    }
+      while(new!=NULL){
+        printf("%d \n", new->data);
+        new=new->next;
+      }
+
+        return 0;
+
+  }
+
+    int main(){
+
+    int value,i;
     int list[5];
-    for (int a = 0;a < 5;a++) {
-        printf("PLease enter the value: \n");
-        scanf("%d", &list[a]);
+
+    for(int a=0;a<5;a++) {
+
+        printf("Enter a value: ");
+        scanf("%d" , &list[a]);
         insert(list[a]);
     }
-    display();
+   
+        display(); //bağlı listeyi yazdırma
 
-    return 0;
-}
+        return 0;
+    }
