bc6ab6270beca0c748916711c8193e23cbdf18af
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-06 14:49:46 +0300

    Update Linkedlist.c

--- Linkedlist.c
+++ Linkedlist.c
@@ -1,32 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct list{                                     //bağlı liste tanımı
+    typedef struct list{                                     //bağlı liste tanımı
     int data;
     struct list* next;
-} list;
+    } list;
 
-list* first=NULL;
-list* last=NULL;
+    list* first=NULL;
+    list* last=NULL;
 
-int insert(int value){                         //bağlı listeye ekleme yapma ve size güncelleme
+    int insert(int value){                                     //bağlı listeye ekleme yapma ve size güncelleme
     list* new = (list*)malloc(sizeof(list));
-    if (new == NULL){
+        
+        if (new == NULL){
         printf("Memory failed \n");
         return -1;
     }
-
+        
     new->data=value;
-        new->next=NULL;
+    new->next=NULL;
 
+        
     if(first != NULL){    //liste boş değilse ekle ve son elemanı güncelle
     last->next=new;
     last=new; 
+        
     }
 
-    else{                 // liste boşsa ilk elemanı ekle
+    else {                 // liste boşsa ilk elemanı ekle
     first =new;
     last=new;
+        
     }
 
     return 0;
@@ -37,7 +41,7 @@ int insert(int value){                         //bağlı listeye ekleme yapma ve
         list* new;
         new= first;
 
-     if (new==NULL){
+     if(new==NULL){
         printf("List is empty \n");
         return -1;
     }
@@ -47,7 +51,6 @@ int insert(int value){                         //bağlı listeye ekleme yapma ve
       }
 
         return 0;
-
   }
 
     int main(){
@@ -62,7 +65,6 @@ int insert(int value){                         //bağlı listeye ekleme yapma ve
         insert(list[a]);
     }
    
-        display(); //bağlı listeyi yazdırma
-
+        display();  
         return 0;
     }
